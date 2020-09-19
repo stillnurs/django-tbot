d674d5a2eb5da04e719851c8651829c5af180d36
stillnurs <noorsultan.mamataliev@gmail.com>
2020-09-20 02:07:30 +0600

    first commit, created app, added models, added admin panel

---
+++ pithub/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PithubConfig(AppConfig):
+    name = 'pithub'
---
+++ pithub/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    external_id = models.PositiveIntegerField(
+        verbose_name='ID пользователя в соц сети',
+    )
+    name = models.TextField(
+        verbose_name='Имя пользователя',
+    )
+
+    def __str__(self):
+        return f'#{self.external_id} {self.name}'
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+
+class Message(models.Model):
+    profile = models.ForeignKey(
+        to='pithub.Profile',
+        verbose_name='Профиль',
+        on_delete=models.PROTECT,
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Время получения',
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f'Сообщение {self.pk} от {self.profile}'
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
